4151519a8be78c9b15ba9a0d00a4d11d4951aef3
sekar <sekarb84@gmail.com>
2022-05-21 14:08:15 +0530

    Sample

---
+++ src/main/java/test1/Sample1.java
@@ -0,0 +1,47 @@
+package test1;
+
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample1 {
+
+	public WebDriver  driver; 
+
+	@BeforeTest
+	public void setup() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();	
+	}
+
+	@Test	
+	public void firstProgram() throws InterruptedException
+	{
+
+		driver.get("https://google.com");
+		String Title = 	driver.getTitle();
+		System.out.println( Title);
+		driver.manage().window().maximize();
+	    Thread.sleep(5); 
+	    
+	}
+	@AfterTest(enabled= false)
+	public void closebrowser() {
+		driver.quit();
+		System.out.println("The driver has been closed.");
+	}
+
+
+
+
+
+}
